5eb0bdb0e253103fb73fce92181837c3ea03e584
Targeter <126644386+Targter@users.noreply.github.com>
2024-06-24 03:05:48 +0530

    Add files via upload

--- src/Component/Box1Container.jsx
+++ src/Component/Box1Container.jsx
@@ -142,24 +142,28 @@ const Box1Container = () => {
                 href="#Home"
                 class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
                 aria-current="page"
+                onClick={ButtonClick}
               >
                 Dashboard
               </a>
               <a
                 href="#Skills"
                 class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                onClick={ButtonClick}
               >
                 Skills
               </a>
               <a
                 href="#WorkExperience"
                 class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                onClick={ButtonClick}
               >
                 Work Experience
               </a>
               <a
                 href="#Projects"
                 class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                onClick={ButtonClick}
               >
                 Projects
               </a>
