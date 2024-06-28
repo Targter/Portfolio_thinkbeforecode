ad18befe0f012a0f5a25f1e586b30e7beccd2c78
Targeter <126644386+Targter@users.noreply.github.com>
2024-06-28 19:03:36 +0530

    Update Box4Geometry.jsx

--- src/Component/Box4Geometry.jsx
+++ src/Component/Box4Geometry.jsx
@@ -6,15 +6,22 @@ import CanvasLoader from "./CanvasLoader";
 import Ball from "./ModelBall";
 
 import { Preload } from "@react-three/drei";
+import { div } from "three/examples/jsm/nodes/Nodes.js";
 // const BallLoad = lazy(() => import("./ModelBall"));
 const Box4Geometry = ({ imgUrl }) => {
   // const memoizedBall = useCallback(() => <Ball imgUrl={imgUrl} />, [imgUrl]);
+
+  const abcale = () => {
+    console.log("error occured");
+    return <div>Canvas is not loaded</div>;
+  };
   return (
     <>
       <Canvas
         frameloop="demand"
         dpr={[1, 2]}
         gl={{ preserveDrawingBuffer: true }}
+        onError={abcale}
       >
         {/* <Suspense fallback={<div>LoadingBalls......</div>}> */}
         <ambientLight intensity={0.1} />
