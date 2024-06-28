7a0fbac625ff859b213b781974843326f12557b2
Targeter <126644386+Targter@users.noreply.github.com>
2024-06-28 18:53:55 +0530

    Update box1a.jsx

--- src/Component/box1a.jsx
+++ src/Component/box1a.jsx
@@ -103,7 +103,7 @@ const Box1a = () => {
 
       {/* <Suspense fallback={<div> LoadingModel.....</div>}> */}
       <Canvas
-        frameloop="always"
+        frameloop="demand"
         shadows
         dpr={[1, 2]}
         camera={{ position: [20, 0, -2.5], fov: 25 }}
